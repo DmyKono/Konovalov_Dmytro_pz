5da4bd5d4d8884e59f51c4d5a18c4be802e2d3ed
DmyKono <159040239+DmyKono@users.noreply.github.com>
2024-03-13 10:25:33 +0200

    Add files via upload

---
+++ ConsoleInterface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD_24
+{
+    public class ConsoleInterface
+    {
+        public void UIrun()
+        {
+            string UserInput = "0";
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("" +
+               "\nChoose the task:" +
+               "\n=====================" +
+               "\n[1] - pz2" +
+               "\n[2] - pz3" +
+               "\n[0] - exit" +
+               "\n=====================" +
+               "\n");
+                UserInput = Convert.ToString(Console.ReadLine());
+                switch (UserInput)
+                {
+                    case "1":
+                        //UIpz2 pz2 = new UIpz2();
+                        //pz2.UIrun();
+                        Console.ReadKey();
+                        continue;
+                    case "2":
+                        //UIpz3 pz3 = new UIpz3();
+                        //pz3.UIrun();
+                        Console.ReadKey();
+                        continue;
+                    case "0":
+                        break;
+                    default:
+                        continue;
+
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using PD_24;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleInterface ConsoleUI = new ConsoleInterface();
+            ConsoleUI.UIrun();
+
+        }
+    }
+}
